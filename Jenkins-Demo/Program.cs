44edcbec8a8870802327efa78dbbd29be502ad5a
Miriam Terrazas <miriamadriana@honeywell.com>
2022-10-08 11:37:03 -0500

    Test one

--- Jenkins-Demo/Program.cs
+++ Jenkins-Demo/Program.cs
@@ -6,7 +6,7 @@ namespace Jenkins_Demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
